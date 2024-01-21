c0bdf9823f1ceb1e753f2ad6ce456d52d57fc08b
Zooya <kyoshida2009@yahoo.co.jp>
2024-01-21 16:48:07 +0900

    Update test.kt

--- test.kt
+++ test.kt
@@ -4,25 +4,26 @@
  */
 
 
-fun readList(members: List<String>) {
+fun <A>readList(members: List<A>) {
   for( person in members ) {
     println( person )
   }
 }
 
 fun main() {
-    var a = arrayOf(1,2,3,7)
-    var n = "こんにちは${a[3]}これが最初の文字${a.size}"
-    var n1 = """
-    	|こんにちは
-		|これが最初の文字
-        |なんですよ
-    """.trimMargin()
-    println(n)
-    println(n1)
+//     var a = arrayOf(1,2,3,7)
+//     var n = "こんにちは${a[3]}これが最初の文字${a.size}"
+//     var n1 = """
+//     	|こんにちは
+// 		|これが最初の文字
+//         |なんですよ
+//     """.trimMargin()
+//     println(n)
+//     println(n1)
 
     val members = listOf("Tom", "Jerry" ,"Bob", "Olibia" )
     val numbers = listOf( 1, 3, 5, 7 )
 
     readList( members )
+    readList( numbers )
 }
