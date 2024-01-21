ba68c8fdbd787f6268dabf68da34f5bd1e19fecf
Zooya <kyoshida2009@yahoo.co.jp>
2024-01-21 17:36:05 +0900

    Update class_test.kt

--- class_test.kt
+++ class_test.kt
@@ -1,12 +1,24 @@
 class Photo {
-  private var imgName: String = "image.jpg"
-  
-  fun getImageName() {
-	println("${imgName}")
-  }
+    var imgName: String = "image.jpg"
+  		set(value) {
+            println("start set imgName")
+    		field = value + ".jpg"        
+        }
+
+    fun getImageName() {
+		println(imgName)
+	}
+    
+    fun setImageName(name: String) {
+        println("start setImageName")
+        imgName = name + ".png"
+    }
 }
 
 fun main() {
     var photo = Photo()
     photo.getImageName()
+    photo.imgName = "image2"
+    photo.setImageName("image3")
+	println(photo.imgName)
 }
